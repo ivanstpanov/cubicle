fa0fc6b79844e6a79c144a5933600898049f05be
ivanstpanov <78256800+ivanstpanov@users.noreply.github.com>
2021-01-31 12:37:35 +0200

    add about controller

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const config = require('./config/config');
+const config = require('./config/config'); 
+const routes = require('./routes');
 const app = express();  
 //expressConfig(app);
 //const expressConfig = require('./config/express');\
 require('./config/express')(app); 
 
-app.get('/', (req,res) => {
-    res.render('home' , {layout : false});
-})
+app.use(routes);
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
