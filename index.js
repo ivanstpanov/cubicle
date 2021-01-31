4f50a563bb7550538aea2d2a0504123f6a850bc4
ivanstpanov <78256800+ivanstpanov@users.noreply.github.com>
2021-01-31 07:48:20 +0200

    extract express config

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const express = require('express');
-
-const config = require('./config/config')
-const handlebars = require('express-handlebars');
-
+const config = require('./config/config');
 const app = express();  
-
-app.engine('hbs',handlebars({
-    extname: 'hbs'
-})); 
-
-app.set('view engine', 'hbs'); 
-app.use(express.static('public'));
+//expressConfig(app);
+//const expressConfig = require('./config/express');\
+require('./config/express')(app); 
 
 app.get('/', (req,res) => {
     res.render('home' , {layout : false});
 })
-console.log(process.env.NODE_ENV);
-
 
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
