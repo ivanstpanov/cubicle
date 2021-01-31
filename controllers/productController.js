0e278101243d63a666f4287a0582713cdf50ca41
ivanstpanov <78256800+ivanstpanov@users.noreply.github.com>
2021-01-31 13:10:14 +0200

    add layout to project

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,10 +2,10 @@ const { Router } = require('express');
 const router = Router(); 
 
 router.get('/', (req, res) => {
-    res.render('home', {layout: false});
+    res.render('home');
 }); 
 router.get('/create', (req, res) => {
-    res.render('create', {layout: false});
+    res.render('create');
 }); 
 
 module.exports = router;
--- routes.js
+++ routes.js
@@ -8,6 +8,6 @@ const router = Router();
 router.use('/about', aboutController); 
 router.use('/', productController); 
 router.get('*', (req,res) => {
-    res.render('404', {layout: false});
+    res.render('404');
 })
 module.exports = router;
