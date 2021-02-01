f75456227a131e9857373c0dbfa97c698d0687c2
ivanstpanov <78256800+ivanstpanov@users.noreply.github.com>
2021-02-01 23:01:21 +0200

    add search

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,20 +2,24 @@ const { Router } = require('express');
 const router = Router();  
 const productService = require('../services/productServices')
 router.get('/', (req, res) => { 
-    let products = productService.getAll();
+
+    let products = productService.getAll(req.query);
     res.render('home', {title :'Browse' , products});
 }); 
 router.get('/create', (req, res) => {
     res.render('create', {title :'Create'});
 }); 
 router.post('/create', validateProduct, (req,res) => {  
-    productService.create(req.body, (err) => { 
-        if (err) {
-            return res.status('500').end();
-        }
-        res.redirect('/products')
-    })
-    res.redirect('/products')
+    // productService.create(req.body, (err) => { 
+    //     if (err) {
+    //         return res.status('500').end();
+    //     }
+    //     res.redirect('/products')
+    // })
+    // res.redirect('/products') 
+    productService.create(req.body)
+    .then(() => res.redirect('/products'))
+    .catch(() => res.status(500).end());
 })
 router.get('/details/:productId', (req, res) => { 
     let product = productService.getOne(req.params.productId);
--- services/productServices.js
+++ services/productServices.js
@@ -1,10 +1,20 @@
 const uniqid = require('uniqid')
 const Cube = require('../models/Cube');  
-const fs = require('fs'); 
+const fs = require('fs/promises'); 
 const productsData = require('../config/products.json');
 
-function getAll(){
-    return productsData;
+function getAll(query){ 
+    let result = productsData; 
+    if (query.search) {
+        result = result.filter(x => x.name.toLowerCase().includes(query.search));
+    } 
+    if (query.from) {
+        result = result.filter(x => Number(x.level) >= query.from)
+    } 
+    if (query.to) {
+        result = result.filter(x => Number(x.level) <= query.to)
+    }
+    return result;
 } 
 function getOne(id){
     return productsData.find(x => x.id == id);
@@ -18,7 +28,10 @@ function create(data,callback){
     data.difficultyLevel
     ); 
     productsData.push(cube); 
-    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData),callback);
+    // fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData),callback); 
+    return fs.writeFile(
+        __dirname + '/../config/products.json', JSON.stringify(productsData)
+    )
     } 
 module.exports = {
     create, 
