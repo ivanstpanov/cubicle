bfe2fbf50b4b0eacc3cab16ab6423e99c7f0cc7c
ivanstpanov <78256800+ivanstpanov@users.noreply.github.com>
2021-02-01 21:58:49 +0200

    save data to json file

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,18 @@
 const { Router } = require('express'); 
-const router = Router(); 
-
+const router = Router();  
+const productService = require('../services/productServices')
 router.get('/', (req, res) => {
     res.render('home', {title :'Browse'});
 }); 
 router.get('/create', (req, res) => {
     res.render('create', {title :'Create'});
 }); 
+router.post('/create', (req,res) => {  
+    productService.create(req.body)
+    res.redirect('/products')
+})
 router.get('/details/:productId', (req, res) => {
     res.render('details', {title :'Details'});
 }); 
-router.post('/create', (req,res) => {
-    console.log(req.body); 
-    res.send('created')
-})
+
 module.exports = router;
